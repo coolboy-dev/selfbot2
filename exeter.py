eed5a44b0b485ea58a7db533faef674ea4e424fb
coolboy-dev <72911172+coolboy-dev@users.noreply.github.com>
2021-02-13 00:51:18 +0530

    Update exeter.py

--- exeter.py
+++ exeter.py
@@ -13,7 +13,6 @@ import time
 from urllib import parse, request
 from itertools import cycle
 from bs4 import BeautifulSoup as bs4
-
 import aiohttp
 import colorama
 import discord
@@ -25,20 +24,19 @@ from discord import Permissions
 from discord.ext import commands
 from discord.utils import get
 from gtts import gTTS
+from keep_alive import keep_alive 
 
 
 class SELFBOT():
     __version__ = 1
 
 
-with open('config.json') as f:
-    config = json.load(f)
 
-token = config.get('token')
-password = config.get('password')
-prefix = config.get('prefix')
+token = ("token.exe")
+password = ('COOLBOY')
+prefix = ('.')
 
-nitro_sniper = config.get('nitro_sniper')
+nitro_sniper = ('True')
 
 stream_url = "https://www.twitch.tv/souljaboy"
 tts_language = "en"
@@ -136,9 +134,8 @@ def Clear():
 
 Clear()
 
-
 def Init():
-    token = config.get('token')
+    token = ("token.exe")
     try:
         Exeter.run(token, bot=False, reconnect=True)
         os.system(f'title (Exeter Selfbot) - Version {SELFBOT.__version__}')
@@ -190,7 +187,7 @@ def RandString():
 
 colorama.init()
 Exeter = discord.Client()
-Exeter = commands.Bot(description='Exeter Selfbot', command_prefix=prefix, self_bot=True)
+Exeter = commands.Bot(description='COOLBOY Selfbot', command_prefix=prefix, self_bot=True)
 
 Exeter.antiraid = False
 Exeter.msgsniper = True
@@ -267,7 +264,7 @@ async def on_message(message):
         if nitro_sniper:
             start = datetime.datetime.now()
             code = re.search("discord.gift/(.*)", message.content).group(1)
-            token = config.get('token')
+            token = ("mfa.WLjWQ7X2C-2Bx4CO4N2iUh51zbawUPTRkPQ2IHsPIipdHhFaV1r6IBnb0KY5qsCzcEEChaI7X6t7IKrWMMqL")
 
             headers = {'Authorization': token}
 
@@ -829,10 +826,10 @@ async def help(ctx, category=None):
     await ctx.message.delete()
     if category is None:
         embed = discord.Embed(color=0xFF633B, timestamp=ctx.message.created_at)
-        embed.set_author(name="𝙀𝙓𝙀𝙏𝙀𝙍 𝙎𝙀𝙇𝙁𝘽𝙊𝙏 | 𝙋𝙍𝙀𝙁𝙄𝙓: " + str(Exeter.command_prefix),
+        embed.set_author(name="𝐂𝐎𝐎𝐋𝐁𝐎𝐘 𝐒𝐄𝐋𝐅𝐁𝐎𝐓 | 𝙋𝙍𝙀𝙁𝙄𝙓: " + str(Exeter.command_prefix),
                          icon_url=Exeter.user.avatar_url)
         embed.set_thumbnail(url=Exeter.user.avatar_url)
-        embed.set_image(url="https://cdn.discordapp.com/attachments/723250694118965300/723253781873164298/image1.gif")
+        embed.set_image(url="https://media.discordapp.net/attachments/806560802177810486/806563870193221652/2cuDwqMjdOk.png?width=747&height=498")
         embed.add_field(name="\uD83E\uDDCA `HELP GENERAL`", value="Shows all general commands", inline=False)
         embed.add_field(name="\uD83E\uDDCA `HELP ACCOUNT`", value="Shows all account commands", inline=False)
         embed.add_field(name="\uD83E\uDDCA `HELP TEXT`", value="Shows all text commands", inline=False)
@@ -845,48 +842,48 @@ async def help(ctx, category=None):
         await ctx.send(embed=embed)
     elif str(category).lower() == "general":
         embed = discord.Embed(color=random.randrange(0x1000000), timestamp=ctx.message.created_at)
-        embed.set_image(url="https://cdn.discordapp.com/attachments/723250694118965300/723272273888280576/image0.gif")
+        embed.set_image(url="https://media.discordapp.net/attachments/806560802177810486/806563870193221652/2cuDwqMjdOk.png?width=747&height=498")
         embed.description = f"\uD83D\uDCB0 `GENERAL COMMANDS`\n`> help <category>` - returns all commands of that category\n`> uptime` - return how long the selfbot has been running\n`> prefix <prefix>` - changes the bot's prefix\n`> ping` - returns the bot's latency\n`> av <user>` - returns the user's pfp\n`> whois <user>` - returns user's account info\n`> tokeninfo <token>` - returns information about the token\n`> copyserver` - makes a copy of the server\n`> rainbowrole <role>` - makes the role a rainbow role (ratelimits)\n`> serverinfo` - gets information about the server\n`> serverpfp` - returns the server's icon\n`> banner` - returns the server's banner\n`> shutdown` - shutsdown the selfbot\n`> getroles` - lists all roles on the server"
         await ctx.send(embed=embed)
     elif str(category).lower() == "account":
         embed = discord.Embed(color=random.randrange(0x1000000), timestamp=ctx.message.created_at)
-        embed.set_image(url="https://cdn.discordapp.com/attachments/723250694118965300/723266223554691202/image0.gif")
+        embed.set_image(url="https://media.discordapp.net/attachments/806560802177810486/806563870193221652/2cuDwqMjdOk.png?width=747&height=498")
         embed.description = f"\uD83D\uDCB0 `ACCOUNT COMMANDS`\n`> ghost` - makes your name and pfp invisible\n`> pfpsteal <user>` - steals the users pfp\n`> setpfp <link>` - sets the image-link as your pfp\n`> hypesquad <hypesquad>` - changes your current hypesquad\n`> spoofcon <type> <name>` - spoofs your discord connection\n`> leavegroups` - leaves all groups that you're in\n`> cyclenick <text>` - cycles through your nickname by letter\n`> stopcyclenick` - stops cycling your nickname\n`> stream <status>` - sets your streaming status\n`> playing <status>` - sets your playing status\n`> listening <status>` - sets your listening status\n`> watching <status>` - sets your watching status\n`> stopactivity` - resets your status-activity\n`> acceptfriends` - accepts all friend requests\n`> delfriends` - removes all your friends\n`> ignorefriends` - ignores all friends requests\n`> clearblocked` - clears your block-list\n`> read` - marks all messages as read\n`> leavegc` - leaves the current groupchat\n`> adminservers` - lists all servers you have perms in\n`> slotbot <on/off>` - snipes slotbots ({Exeter.slotbot_sniper})\n`> giveaway <on/off>` - snipes giveaways ({Exeter.giveaway_sniper})\n`> mee6 <on/off>` - auto sends messages in the specified channel ({Exeter.mee6}) <#{Exeter.mee6_channel}>\n`> yuikiss <user>` - auto sends yui kisses every minute <@{Exeter.yui_kiss_user}> <#{Exeter.yui_kiss_channel}>\n`> yuihug <user>` - auto sends yui hugs every minute <@{Exeter.yui_hug_user}> <#{Exeter.yui_hug_channel}>\n`> yuistop` - stops any running yui loops"
         await ctx.send(embed=embed)
     elif str(category).lower() == "text":
         embed = discord.Embed(color=random.randrange(0x1000000), timestamp=ctx.message.created_at)
-        embed.set_image(url="https://cdn.discordapp.com/attachments/723250694118965300/723278609648713818/image0.gif")
+        embed.set_image(url="https://media.discordapp.net/attachments/806560802177810486/806563870193221652/2cuDwqMjdOk.png?width=747&height=498")
         embed.description = f"\uD83D\uDCB0 `TEXT COMMANDS`\n`> exeter` - sends the exeter logo\n`> snipehistory` - shows a history of deleted messages\n`> clearsnipehistory` - clears snipe history of current channel\n`> snipe` - shows the last deleted message\n`> editsnipe` - shows the last edited message\n`> msgsniper <on/off> ({Exeter.msgsniper})` - enables a message sniper for deleted messages in DMs\n`> clear` - sends a large message filled with invisible unicode\n`> del <message>` - sends a message and deletes it instantly\n`> 1337speak <message>` - talk like a hacker\n`> minesweeper` - play a game of minesweeper\n`> spam <amount>` - spams a message\n`> dm <user> <content>` - dms a user a message\n`> reverse <message>` - sends the message but in reverse-order\n`> shrug` - returns ¯\_(ツ)_/¯\n`> lenny` - returns ( ͡° ͜ʖ ͡°)\n`> fliptable` - returns (╯°□°）╯︵ ┻━┻\n`> unflip` - returns (╯°□°）╯︵ ┻━┻\n`> bold <message>` - bolds the message\n`> censor <message>` - censors the message\n`> underline <message>` - underlines the message\n`> italicize <message>` - italicizes the message\n`> strike <message>` - strikethroughs the message\n`> quote <message>` - quotes the message\n`> code <message>` - applies code formatting to the message\n`> purge <amount>` - purges the amount of messages\n`> empty` - sends an empty message\n`> tts <content>` - returns an mp4 file of your content\n`> firstmsg` - shows the first message in the channel history\n`> ascii <message>` - creates an ASCII art of your message\n`> wizz` - makes a prank message about wizzing \n`> 8ball <question>` - returns an 8ball answer\n`> slots` - play the slot machine\n`> everyone` - pings everyone through a link\n`> abc` - cyles through the alphabet\n`> 100` - cycles -100\n`> cum` - makes you cum lol?\n`> 9/11` - sends a 9/11 attack\n`> massreact <emoji>` - mass reacts with the specified emoji"
         await ctx.send(embed=embed)
     elif str(category).lower() == "music":
         embed = discord.Embed(color=random.randrange(0x1000000), timestamp=ctx.message.created_at)
-        embed.set_image(url="https://cdn.discordapp.com/attachments/723250694118965300/723270695185809468/image1.gif")
+        embed.set_image(url="https://media.discordapp.net/attachments/806560802177810486/806563870193221652/2cuDwqMjdOk.png?width=747&height=498")
         embed.description = f"\uD83D\uDCB0 `MUSIC COMMANDS`\n`> play <query>` - plays the specified song if you're in a voice-channel\n`> stop` - stops the music player\n`> skip` - skips the current song playing\n`> lyrics <song>` - shows the specified song's lyrics\n`> youtube <query>` - returns the first youtube search result of the query"
         await ctx.send(embed=embed)
     elif str(category).lower() == "image":
         embed = discord.Embed(color=random.randrange(0x1000000), timestamp=ctx.message.created_at)
         embed.set_image(
-            url="https://cdn.discordapp.com/attachments/723250694118965300/739548124493643837/ezgif.com-video-to-gif.gif")
+            url="https://media.discordapp.net/attachments/806560802177810486/806563870193221652/2cuDwqMjdOk.png?width=747&height=498")
         embed.description = f"\uD83D\uDCB0 `IMAGE MANIPULATION COMMANDS`\n`> tweet <user> <message>` makes a fake tweet\n`> magik <user>` - distorts the specified user\n`> fry <user>` - deep-fry the specified user\n`> blur <user>` - blurs the specified user\n`> pixelate <user>` - pixelates the specified user\n`> Supreme <message>` - makes a *Supreme* logo\n`> darksupreme <message>` - makes a *Dark Supreme* logo\n`> fax <text>` - makes a fax meme\n`> blurpify <user>` - blurpifies the specified user\n`> invert <user>` - inverts the specified user\n`> gay <user>` - makes the specified user gay\n`> communist <user>` - makes the specified user a communist\n`> snow <user>` - adds a snow filter to the specified user\n`> jpegify <user>` - jpegifies the specified user\n`> pornhub <logo-word 1> <logo-word 2>` - makes a PornHub logo\n`> phcomment <user> <message>` - makes a fake PornHub comment\n"
         await ctx.send(embed=embed)
     elif str(category).lower() == "nsfw":
         embed = discord.Embed(color=random.randrange(0x1000000), timestamp=ctx.message.created_at)
-        embed.set_image(url="https://cdn.discordapp.com/attachments/723250694118965300/723268822789783612/image1.gif")
+        embed.set_image(url="https://media.discordapp.net/attachments/806560802177810486/806563870193221652/2cuDwqMjdOk.png?width=747&height=498")
         embed.description = f"\uD83D\uDCB0 `NSFW COMMANDS`\n`> anal` - returns anal pics\n`> erofeet` - returns erofeet pics\n`> feet` - returns sexy feet pics\n`> hentai` - returns hentai pics\n`> boobs` - returns booby pics\n`> tits` - returns titty pics\n`> blowjob` - returns blowjob pics\n`> neko` - returns neko pics\n`> lesbian` - returns lesbian pics\n`> cumslut` - returns cumslut pics\n`> pussy` - returns pussy pics\n`> waifu` - returns waifu pics"
         await ctx.send(embed=embed)
     elif str(category).lower() == "misc":
         embed = discord.Embed(color=random.randrange(0x1000000), timestamp=ctx.message.created_at)
-        embed.set_image(url="https://cdn.discordapp.com/attachments/723250694118965300/723265016979259544/image0.gif")
+        embed.set_image(url="https://media.discordapp.net/attachments/806560802177810486/806563870193221652/2cuDwqMjdOk.png?width=747&height=498")
         embed.description = f"\uD83D\uDCB0 `MISCELLANEOUS COMMANDS`\n`> copycat <user>` - copies the users messages ({Exeter.copycat})\n`> stopcopycat` - stops copycatting\n`> fakename` - makes a fakename with other members's names\n`> geoip <ip>` - looks up the ip's location\n`> pingweb <website-url>` pings a website to see if it's up\n`> anticatfish <user>` - reverse google searches the user's pfp\n`> stealemoji` - <emoji> <name> - steals the specified emoji\n`> hexcolor <hex-code>` - returns the color of the hex-code\n`> dick <user>` - returns the user's dick size\n`> bitcoin` - shows the current bitcoin exchange rate\n`> hastebin <message>` - posts your message to hastebin\n`> rolecolor <role>` - returns the role's color\n`> nitro` - generates a random nitro code\n`> feed <user>` - feeds the user\n`> tickle <user>` - tickles the user\n`> slap <user>` - slaps the user\n`> hug <user>` - hugs the user\n`> cuddle <user>` - cuddles the user\n`> smug <user>` - smugs at the user\n`> pat <user>` - pat the user\n`> kiss <user>` - kiss the user\n`> topic` - sends a conversation starter\n`> wyr` - sends a would you rather\n`> gif <query>` - sends a gif based on the query\n`> sendall <message>` - sends a message in every channel\n`> poll <msg: xyz 1: xyz 2: xyz>` - creates a poll\n`> bots` - shows all bots in the server\n`> image <query>` - returns an image\n`> hack <user>` - hacks the user\n`> token <user>` - returns the user's token\n`> cat` - returns random cat pic\n`> sadcat` - returns a random sad cat\n`> dog` - returns random dog pic\n`> fox` - returns random fox pic\n`> bird` - returns random bird pic\n"
         await ctx.send(embed=embed)
     elif str(category).lower() == "antinuke":
         embed = discord.Embed(color=random.randrange(0x1000000), timestamp=ctx.message.created_at)
-        embed.set_image(url="https://cdn.discordapp.com/attachments/723250694118965300/723274816055935067/image0.gif")
+        embed.set_image(url="https://media.discordapp.net/attachments/806560802177810486/806563870193221652/2cuDwqMjdOk.png?width=747&height=498")
         embed.description = f"\uD83D\uDCB0 `ANTI-NUKE COMMANDS`\n`> antiraid <on/off>` - toggles anti-nuke ({Exeter.antiraid})\n`> whitelist <user>` - whitelists the specified user\n**NOTE** Whitelisting a user will completely exclude them from anti-nuke detections, be weary on who you whitelist.\n`> whitelisted <-g>` - see who's whitleisted and in what guild\n`> unwhitelist <user>` - unwhitelists the user\n`> clearwhitelist` - clears the whitelist hash"
         await ctx.send(embed=embed)
     elif str(category).lower() == "nuke":
         embed = discord.Embed(color=random.randrange(0x1000000), timestamp=ctx.message.created_at)
-        embed.set_image(url="https://cdn.discordapp.com/attachments/723250694118965300/723256768742031451/image0.gif")
+        embed.set_image(url="https://media.discordapp.net/attachments/806560802177810486/806563870193221652/2cuDwqMjdOk.png?width=747&height=498")
         embed.description = f"\uD83D\uDCB0 `NUKE COMMANDS`\n`> tokenfuck <token>` - disables the token\n`> nuke` - nukes the server\n`> massban` - bans everyone in the server\n`> dynoban` - mass bans with dyno one message at a time\n`> masskick` - kicks everyone in the server\n`> spamroles` - spam makes 250 roles\n`> spamchannels` - spam makes 250 text channels\n`> delchannels` - deletes all channels in the server\n`> delroles` - deletes all roles in the server\n`> purgebans` - unbans everyone\n`> renamechannels <name>` - renames all channels\n`> servername <name>` - renames the server to the specified name\n`> nickall <name>` - sets all user's nicknames to the specified name\n`> changeregion <amount>` - spam changes regions in groupchats\n`> kickgc` - kicks everyone in the gc\n`> spamgcname` - spam changes the groupchat name\n`> massmention <message>` - mass mentions random people\n`> giveadmin` - gives all admin roles in the server"
         await ctx.send(embed=embed)
 
@@ -3302,6 +3299,7 @@ async def nitro(ctx):
     await ctx.message.delete()
     await ctx.send(Nitro())
 
+keep_alive()
 
 if __name__ == '__main__':
     Init()
